fd77c179aab1d770b2e1e4f9c35fff935f58cf4c
Ega Sepyana <egasepyana04@gmail.com>
2025-04-17 21:41:45 +0700

    create api

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Dita',
+            'email' => 'dita@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
+
